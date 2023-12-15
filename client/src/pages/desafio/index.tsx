0e8252708e823d97635a1638569613f8acde5557
Thaís Neves <neves.thais@hotmail.com>
2023-12-15 12:56:41 -0300

    added forgot password

--- client/src/pages/desafio/index.tsx
+++ client/src/pages/desafio/index.tsx
@@ -14,11 +14,14 @@ export default function Desafio() {
                 <div style={{width: "384px", height: "427px", display: "flex", flexDirection: "column", alignItems: "flex-start", marginTop: "121px", marginLeft: "112px"}}>
                     <Typography variant="h4" style={{color:"#1E293B"}}>Acesse a plataforma</Typography>
                     <p style={{marginTop: "16px"}}>Faça login ou registre-se para começar a construir <br /> seus projetos ainda hoje.</p>
-                    <Typography color={"#1E293B"} style={{marginTop: "40px", fontSize: "14px"}}>E-mail</Typography>
+                    <Typography color={"#1E293B"} style={{marginTop: "40px", fontSize: "14px", fontWeight: "600"}}>E-mail</Typography>
                     <TextField placeholder="Digite seu e-mail" style={{color: "#94A3B8", fontSize: "14px", width: "384px"}}></TextField>
-                    <Typography style={{ marginTop: "20px", fontSize: "14px" }} color={"#1E293B"}>Senha</Typography>
+                    <div style={{display: "flex", flexDirection: "row", justifyContent:"space-between", width: "384px", marginTop:"20px", marginBottom: "8px"}}>
+                            <Typography style={{fontSize: "14px", fontWeight: "600" }} color={"#1E293B"}>Senha</Typography>
+                            <a style={{color:"#51E678", fontSize:"14px", fontWeight:"600"}}>Esqueceu a senha?</a>
+                        </div>
                     <TextField placeholder="Digite sua senha" style={{color:"#94A3B8", fontSize: "14px", width: "384px"}}></TextField>
-                    <Button variant="contained" style={{marginTop: "32px", height: "56px", width: "384px", backgroundColor: "#51E678", fontSize: "16px", borderRadius: "4px"}}>Entrar</Button>
+                    <Button variant="contained" style={{ marginTop: "32px", height: "56px", width: "384px", backgroundColor: "#51E678", fontSize: "16px", borderRadius: "4px"}}>Entrar</Button>
                 </div>
             </div>
 
